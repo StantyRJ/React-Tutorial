6aff0ba0df740b4df682f58914f52ac477bc7416
StantyRJ <66645764+StantyRJ@users.noreply.github.com>
2023-08-13 12:51:27 -0700

    finished up to tutorial 12

--- my-app/src/home.js
+++ my-app/src/home.js
@@ -1,18 +1,17 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-
-    const handleClick = () => {
-        setName('luigi');
-    }
+    const [blogs, setBlogs] = useState([
+        {title: 'Batman strikes!', body: 'He is vengangce', author: 'Bruce Wayne', id:1},
+        {title: 'Superman Strikes down Zodd!', body: 'Jimmy olsen the goat', author: 'Clark Kent', id:2},
+        {title: 'Batman strikes again!', body: 'He is vengangce', author: 'Dick Grayson', id:3}
+    ]);
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name}</p>
-            <button onClick={handleClick}>Click me</button>
+            <BlogList blogs={blogs} title="All Blogs!"/>
+            <BlogList blogs={blogs.filter((blog) => blog.title.includes("Batman"))} title="Batman Archives"/>
         </div>
     );
 }
